835ff7b2ec3312bd690d91044438899a9f7d6bc1
sean lin <seanlin0918@gmail.com>
2017-03-11 23:41:09 +1000

    implement groups#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,14 @@ class GroupsController < ApplicationController
     def index
         @groups = Group.all
     end
+
+    def new
+        @group = Group.new
+    end
+
+    def create
+        @group = Group.find(group_params)
+        @group.save
+        redirect_to group_path
+    end
 end
